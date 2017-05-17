8ee10c9172e912014be94423ddab6cc19a1e2769
bhavana94 <bhavanasingh49@gmail.com>
2017-05-17 18:20:09 +0530

    sum of even values terms in fibonacci series

---
+++ EulerProjectSolution/problem2.py
@@ -0,0 +1,17 @@
+"""
+Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
+"""
+
+def fibobacci(n):
+	if n<=1:
+		return n
+	else:
+		return (fibobacci(n-1)+fibobacci(n-2))
+b = []
+for i in range(0,4000000):
+	print fibobacci(i)
+	b.append(fibobacci(i))
+
+print sum(k for k in b if not k%2)
+
+	
